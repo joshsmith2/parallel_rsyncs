dd12677c0a519b37f43dd155b247fdde94b4405d
Josh Smith <joshsmith2@gmail.com>
2015-03-26 18:02:39 +0000

    Added test data

---
+++ parallel_rsyncs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Requirements:
+#
+# ESSENTIAL:
+# - Copy to destination, with full logs on errors etc
+# - --ignore-existing, regular transfer or --delete options
+# - Move or copy options
+# - Ability to specify a binary
+# - Ability to specify how many threads to run
+#
+# NICE BUT NOT STRICTLY NECESSARY:
+# - Ability to specify a file containing paths to move
+
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,5 +1,4 @@
 from base import *
-import main
 
 class UnitTest(GeneralTest):
 
