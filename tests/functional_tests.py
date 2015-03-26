703003a3d8aedfc2ba57c4719afb817bdac7cee2
Josh Smith <joshsmith2@gmail.com>
2015-03-26 17:20:46 +0000

    Added unit testing framework

---
+++ tests/functional_tests.py
@@ -0,0 +1,9 @@
+from base import *
+
+class FunctionalTest(GeneralTest):
+
+    def test_goes_here(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+     unittest.main()
---
+++ tests/unit_tests.py
@@ -0,0 +1,10 @@
+from base import *
+import main
+
+class UnitTest(GeneralTest):
+
+    def test_goes_here(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+     unittest.main()
